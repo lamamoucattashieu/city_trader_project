a395ceabf95ad4da55a2cff63f6aec56afa9d4d2
lamamoucattashieu <lhvmoucattas.ieu2024@student.ie.edu>
2025-12-01 22:42:16 +0100

    Update player.py

--- city_trader/player.py
+++ city_trader/player.py
@@ -1,9 +1,11 @@
 class Player:
     def __init__(self, start_city, fuel=100, money=500):
+        # average and worst case time complexity: O(1)
         self.location = start_city
         self.fuel = fuel
         self.money = money
         self.inventory = {}
 
     def __repr__(self):
+        # average and worst case time complexity: O(1)
         return f"Player(location={self.location}, fuel={self.fuel}, money={self.money})"
